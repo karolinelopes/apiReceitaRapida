a0ec4002c87483c8b71642a164fbf82a20899a17
Karoline Lopes da Silva <69213378+karolinelopes@users.noreply.github.com>
2021-03-29 18:17:11 -0300

    Update recipe-route.js

--- src/routes/recipe-route.js
+++ src/routes/recipe-route.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/recipe-controller');
+const authService = require('../services/auth-service');
 
 router.get('/', controller.get);
 router.get('/:name', controller.getByName);
 router.get('/admin/:id', controller.getById);
-router.post('/', controller.post);
-router.put('/:id', controller.put);
-router.delete('/', controller.delete);
+router.post('/', authService.authorize, controller.post);
+router.put('/:id', authService.isAdmin, controller.put);
+router.delete('/', authService.isAdmin, controller.delete);
 
 
 
